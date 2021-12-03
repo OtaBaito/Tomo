f4361bcd4d4e94d8fdcd9edd919485c0a438c313
Fray117 <5698303-Fray117@users.noreply.gitlab.com>
2021-12-03 08:19:59 +0700

    DYnamic Host

--- routes/web.php
+++ routes/web.php
@@ -19,15 +19,15 @@ use Inertia\Inertia;
 |
 */
 
-Route::domain('go.localhost')->group(function () {
+Route::domain('go.' . config('domain')->group(function () {
 	Route::get('/', [Momo::class, 'home'])->name('momolink');
 });
 
-Route::domain('api.localhost')->group(function () {
+Route::domain('api.' . config('domain')->group(function () {
 	Route::get('/', [Jinja::class, 'home'])->name('jinja');
 });
 
-Route::domain('localhost')->group(function () {
+Route::domain(config('domain')->group(function () {
 	Route::get('/', [Landing::class, 'home'])->name('home');
 	Route::get('/events', [Landing::class, 'events'])->name('events');
 	Route::get('/about', [Landing::class, 'about'])->name('about');
